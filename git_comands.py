3433c9c33ce86f2954b6f51348cbb48d393e36f9
shinobiw <shinobiwb@gmail.com>
2021-05-21 19:17:32 +0600

    initial commit

---
+++ git_comands.py
@@ -0,0 +1,13 @@
+git init -.git
+git remote add name url 
+git pull origin master
+git status
+git add
+git commit -m "comment"
+git branch 
+git branch name branch
+git checkout name barnch
+git push name branch
+git push origin master
+git reset filename
+
